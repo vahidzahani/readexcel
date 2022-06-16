7d037cae94dde4e7b9d7b4dc1040c5beae0676b4
vahid-OFFICE <vahid.zahani@gmail.com>
2022-06-16 08:51:52 +0430

    update new

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from openpyxl import load_workbook
 import re
 
-book = load_workbook('main.xlsx')
+file_excel="main.xlsx"
+file_csv="main.csv"
+
+book = load_workbook(file_excel)
 sheet=book['Sheet1']
 rows=sheet.rows
 head=[cell.value for cell in next(rows)]
 
 extract_phone_number_pattern = "\\+?[1-9][0-9]{7,14}"
-# a=re.findall(extract_phone_number_pattern, 'You can reach me out at +989989978798 and +56667778888') # returns ['+12223334444', '+56667778888']
 
-# print(a[0])
 print("start")
-f=open("main.csv","w")
+f=open(file_csv,"w")
 for now in rows:
     data={}
     for title,cell in zip(head,now):
@@ -20,7 +21,6 @@ for now in rows:
     b=str(data['to'])
     x=re.findall(extract_phone_number_pattern, a) # returns ['+12223334444', '+56667778888']
     y=re.findall(extract_phone_number_pattern, b) # returns ['+12223334444', '+56667778888']
-    #print(x[0] + "," + y[0])
     f.writelines(x[0] + "," + y[0] + "\n")
 f.close()
 print("end")
